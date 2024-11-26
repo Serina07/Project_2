480af41a0d9080fc03dcd6d570a1e2331b1d88a0
Serina07 <serinaprjl@gmail.com>
2024-11-26 09:17:51 +0545

    css done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+
 // import { useState, useEffect } from "react";
 // import { Style } from 'react';
 // import Button from './components/Button/Button';
--- src/components/ingredients/Ingredients.jsx
+++ src/components/ingredients/Ingredients.jsx
@@ -71,105 +71,97 @@ function Ingredients() {
   
   return (
     <>
-    <div className={styles.side}>
-
-    <div className={styles.i_container}>
-
-      <div className={styles.left_wrapper0}>
-
-      <div className={styles.i_tile}>
-        <span className={styles.text}>
-          "WHAT DO YOU HAVE?"
-        </span>
-        <div className={styles.k_container}>
+      <div className={styles.container}>
+        <div className={styles.row}>
+        <div className={`col-md-3 ${styles.customColumn} ${styles.first}`}>
+        <div className={styles.left_wrapper0}>
+          <div className={styles.i_tile}>
+            <span className={styles.text}>
+              "WHAT DO YOU HAVE?"
+            </span>
+          <div className={styles.k_container}>
           <p className={styles.k_button}>
-            Available Ingredients
-          </p>
-
+          Available Ingredients
+          </p></div>
+          <div className={styles.clear}></div>
         </div>
-        <div className={styles.clear}></div>
-      </div>
-      
       <div className={styles.clear}></div>
       <div className={styles.left_wrapper}>
-        <div
-          className={styles.tile_wrapper_tiles}
-          style={{ position: "relative", overflow: "hidden" }}
-        >
-          {ingredients.map((ingredient) => (
-            <div key={ingredient.id} className={styles.i_list}>
-              <span className={styles.check_box}>
-                <input
-                  type="checkbox"
-                  value={ingredient.id}
-                  name="ingredients[]"
-                  className={styles.ingredient_check}
-                />
-                <span  className={styles.ingredient_checkbox}></span>
-                <span className={styles.ingredient_checkbox_label}>
-                  {ingredient.name}
-                </span>
-                <span className={styles.clear}></span>
-              </span>
-            </div>
-          ))}
-        </div>
+      <div className={styles.tile_wrapper_tiles} style={{ position: "relative", overflow: "hidden" }}>
+       {ingredients.map((ingredient) => (
+        <div key={ingredient.id} className={styles.i_list}>
+        <span className={styles.check_box}>
+          <input
+            type="checkbox"
+            value={ingredient.id}
+            name="ingredients[]"
+            className={styles.ingredient_check}
+          />
+          <span  className={styles.ingredient_checkbox}></span>
+          <span className={styles.ingredient_checkbox_label}>
+            {ingredient.name}
+          </span>
+          <span className={styles.clear}></span>
+        </span>
+      </div>
+         ))}
+      </div>
       </div> 
       </div>
-      <div className={styles.checked_ingredients_container}>
-              <div className={styles.checked_ingredients} >
-                  <p className={styles.checked_ingredients_title}>Your Ingredients</p>
-                  <div className={styles.Empty}>
-                      <p>You don't have any selected ingredients.</p>
-                  </div>
-                  <div className={styles.Clear}>
-                      <div className={styles.remove}>
-                          Clear All
-                      </div>
-                  </div>
-                  <div className={styles.List}>
-                      
-                  </div>
-                  <div className={styles.button_submit} onClick="findRecipes()">
-                      <button className={styles.button}>Find Recipes</button>
+      </div>
+      <div className={`col-md-3 ${styles.customColumn} ${styles.second}`}>
+      <div className={styles.checked_ingredients} >
+              <p className={styles.checked_ingredients_title}>Your Ingredients</p>
+              <div className={styles.Empty}>
+                  <p>You don't have any selected ingredients.</p>
+              </div>
+              <div className={styles.Clear}>
+                  <div className={styles.remove}>
+                      Clear All
                   </div>
               </div>
-    </div>
-      </div>
-      <div className={styles.container}>
-      <div className={styles.card}>
-        <h1 className={styles.title}>Food Recipe Detection</h1>
-        <div className={styles.uploadArea}>
-          <label htmlFor="upload" className={styles.uploadLabel}>
-            Click here to upload an image of food
-          </label>
-          <input
-            id="upload"
-            type="file"
-            accept="image/*"
-            onChange={handleImageUpload}
-            className={styles.uploadInput}
-          />
-        </div>
-        {image && (
-          <img src={image} alt="Uploaded food" className={styles.previewImage} />
-        )}
-        {isDetecting && (
-          <div className={styles.detectionResult}>
-            <div className={styles.loadingSpinner}></div>
-            <p>Detecting recipe...</p>
+              <div className={styles.List}>
+                  
+              </div>
+              <div className={styles.button_submit} onclick="findRecipes()">
+                  <button className={styles.button}>Find Recipes</button>
+              </div>
           </div>
-        )}
-        {detectedRecipe && (
-          <button className={styles.detectionResult}>
-            <p>Recipe {detectedRecipe}</p>
-          </button>
-        )}
+        </div>
+    <div className={`col-md-3 ${styles.customColumn} ${styles.third}`}>
+    <div className={styles.card}>
+    <h1 className={styles.title}>Food Recipe Detection</h1>
+    <div className={styles.uploadArea}>
+      <label htmlFor="upload" className={styles.uploadLabel}>
+        Click here to upload an image of food
+      </label>
+      <input
+        id="upload"
+        type="file"
+        accept="image/*"
+        onChange={handleImageUpload}
+        className={styles.uploadInput}
+      />
+    </div>
+    {image && (
+      <img src={image} alt="Uploaded food" className={styles.previewImage} />
+    )}
+    {isDetecting && (
+      <div className={styles.detectionResult}>
+        <div className={styles.loadingSpinner}></div>
+        <p>Detecting recipe...</p>
       </div>
-    </div>
-    </div>
-    
-      </>
+    )}
+    {detectedRecipe && (
+      <button className={styles.detectionResult}>
+        <p>Recipe {detectedRecipe}</p>
+      </button>
+    )}
+  </div>
+  </div>
+ </div>
+</div>
+    </>
   );
 }
 
